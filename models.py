ef5b26a49692286156d3b003010d58ddaaea15b8
Timur Pustoplesnov <timur.pustoplesnov@yandex.ru>
2024-08-06 23:52:28 +0300

    update project 06-08-2024

--- models.py
+++ models.py
@@ -4,11 +4,27 @@ from sqlalchemy.orm import declarative_base, relationship
 Base = declarative_base()
 
 class Words(Base):
-    __tablename__ = 'words'
+    __tablename__ = 'tg_words'
 
     id = sq.Column(sq.Integer, primary_key=True)
     russian_word = sq.Column(sq.String(length=100), unique=True)
     current_english_word = sq.Column(sq.String(length=100), unique=True)
-    fake_english_translate_one = sq.Column(sq.String(length=100), nullable=False)
-    fake_english_translate_two = sq.Column(sq.String(length=100), nullable=False)
-    fake_english_translate_three = sq.Column(sq.String(length=100), nullable=False)
+
+
+class Facts(Base):
+    __tablename__ = 'tg_facts'
+
+    id_operation = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey('tg_users.id'), nullable=False)
+    word_id = sq.Column(sq.Integer, sq.ForeignKey('tg_words.id'), nullable=False)
+    answer = sq.Column(sq.String(length=100), nullable=False)
+    date_time = sq.Column(sq.DateTime, nullable=False)
+
+
+class Users(Base):
+    __tablename__ = 'tg_users'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
